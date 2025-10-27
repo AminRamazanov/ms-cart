3496d0672153173857cca6fe16b0e6497b45709d
Amin Ramazanov <ramazanov.amiin@gmail.com>
2025-10-27 23:48:00 +0400

    application.yml fixed

--- src/main/java/com/example/mscart/client/ProductClient.java
+++ src/main/java/com/example/mscart/client/ProductClient.java
@@ -8,7 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @FeignClient(
         name = "product",
-        url = "${client.catalog-service.url}/product",
+        url = "${client.catalog-service.url}",
         configuration = FeignClientConfig.class)
 public interface ProductClient {
     @GetMapping("/{id}")
